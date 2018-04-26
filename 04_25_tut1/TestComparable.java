ee6966a5d71bc337a7a2aaa9f673e28d386e1f00
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-04-26 03:36:09 +0200

    version2.1

--- 04_25_tut1/TestComparable.java
+++ 04_25_tut1/TestComparable.java
@@ -3,7 +3,7 @@ public class TestComparable{
         Comparable c = new Rechteck(2.0, 4.0);
         Rechteck r = new Rechteck(3.0, 2.0);
         Object o = new Rechteck(4.0, 2.0);
-
+        System.out.println("Compare");
         System.out.println("o, c " + ((Rechteck)o).compareTo(c));
         System.out.println("r, c " + r.compareTo(c));  
     }
