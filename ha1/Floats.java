d888eb82a88d212e35cc8cd09a366a010505889c
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-04-26 03:51:58 +0200

    1.0

---
+++ ha1/Floats.java
@@ -0,0 +1,19 @@
+public class Floats{
+    private float[] werte;
+    private float kleinstes;
+
+    public Floats(float[] werte){
+        this.werte = werte;
+    }
+
+    public float kleinere(float x, float y){
+        if(Math.abs(x) < Math.abs(y)){
+            return x;
+        }
+        else return y;
+    }
+
+    public void setKleinstes(float kleinstes){
+        
+    }
+}
